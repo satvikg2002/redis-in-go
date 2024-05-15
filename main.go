8e1c009861b5e758bf3cf6a9b875a74e68430614
Satvik <satvik.noida@gmail.com>
2024-05-15 21:55:15 +0530

    redis command handler

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -25,16 +26,38 @@ func main() {
 	defer conn.Close()
 
 	for {
-		resp := NewResp(conn)
-		value, err := resp.Read()
-		if err != nil {
-			fmt.Println(err)
-			return
+		for {
+			resp := NewResp(conn)
+			value, err := resp.Read()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		
+			if value.typ != "array" {
+				fmt.Println("Invalid request, expected array")
+				continue
+			}
+		
+			if len(value.array) == 0 {
+				fmt.Println("Invalid request, expected array length > 0")
+				continue
+			}
+		
+			command := strings.ToUpper(value.array[0].bulk)
+			args := value.array[1:]
+		
+			writer := NewWriter(conn)
+		
+			handler, ok := Handlers[command]
+			if !ok {
+				fmt.Println("Invalid command: ", command)
+				writer.Write(Value{typ: "string", str: ""})
+				continue
+			}
+		
+			result := handler(args)
+			writer.Write(result)
 		}
-
-		_ = value
-
-		writer := NewWriter(conn)
-		writer.Write(Value{typ: "string", str: "OK"})
 	}
 }
