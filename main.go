3426ce23305ac831c02d802c30fef9ca4ca1d533
Satvik <satvik.noida@gmail.com>
2024-05-15 21:39:46 +0530

    read resp

--- main.go
+++ main.go
@@ -2,44 +2,39 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
-	"os"
 )
 
 func main() {
 	fmt.Println("Listening on port :6379")
 
-	// TCP Listener
+	// Create a new server
 	l, err := net.Listen("tcp", ":6379")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// Rx requests
+	// Listen for connections
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	defer conn.Close()	// close connection once finished
+	defer conn.Close()
 
 	for {
-		buf := make([]byte, 1024)
-
-		// read mssg from client
-		_, err = conn.Read(buf)
+		resp := NewResp(conn)
+		value, err := resp.Read()
 		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			fmt.Println("error reading from client: ", err.Error())
-			os.Exit(1)
+			fmt.Println(err)
+			return
 		}
 
-		// ignore request ans send back acknowledgement
+		fmt.Println(value)
+
+		// ignore request and send back a PONG
 		conn.Write([]byte("+OK\r\n"))
 	}
 }
